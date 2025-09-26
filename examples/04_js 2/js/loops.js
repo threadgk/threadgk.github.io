a7663ad0cd25bd9ba9b6c3c2fc5c458a19efcaeb
Kayla Threadgill <krthread@gmail.com>
2025-09-25 23:14:38 -0400

    add 07 to home page

--- examples/04_js 2/js/loops.js
+++ examples/04_js 2/js/loops.js
@@ -29,4 +29,66 @@ document.getElementById("btn-count").onclick = () => {
         return;
     }
 
- 
+    //loop for every number from start to end and display the number in paragraphs 
+
+    for(let i=startNum; i<= endNum; i++){
+        const p = document.createElement.p;
+        p.innerHTML=i;
+        resultDiv.append(p);
+        p.onclick = () => {
+            console.log(`You clicked Number ${p}`);
+        }
+    }
+};
+
+
+document.getElementById("btn-show-toys").onclick = () => {
+    const toyList = document.getElementById("toy-list");
+    toyList.innerHTML = ""; 
+
+    const toys = ["ball","skipping rope","doll","mini car","elmo"];
+    /* first way to iterate 
+
+    for(let i=0; i<toys.length; i++){
+        const li = document.createElement("li");
+        li.innerHTML= toys[i];
+        toys[i]= toyList.append(li);
+    }*/
+
+        /* second way */
+        toys.forEach((toy, i)=>{
+            console.log(i+"."+toy);
+
+            const li= document.createElement("li");
+            li.innerHTML = toy;
+            toyList.append(li);
+        }); 
+}
+
+document.getElementById("btn-show-toy-prices").onclick = () => {
+
+    const toys = [];
+    toys["ball"] = 2.99;
+    toys["barbie"] = 19.99;
+    toys["skipping rope"] = 1.98;
+    toys["match box car"] = 2.54; 
+
+    const toyDiv = document.getElementById("toy-prices");
+    toyDiv.innerHTML = ""; 
+
+    const priceP = document.createElement("p");
+    priceP.innerHTML = "";
+
+    for(let toy in toys){
+        const p = document.createElement("p");
+        p.innerHTML= toy;
+        toyDiv.append(p);
+
+        // when i click the paragraph ..... in a div below show the price 
+        // like "Ball costs $..." 
+    priceP.onclick = () => {
+    priceP.inneraHTML = `${toy} costs $${toys[toy]}.`
+    }
+
+}
+}

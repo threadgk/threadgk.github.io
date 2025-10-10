a57545e60209c6278c4e69889e665ec7de92fe45
Kayla Threadgill <krthread@gmail.com>
2025-10-09 23:46:31 -0400

    updating website with JSON files for Project part 6

--- projects/part6/js/home-page.js
+++ projects/part6/js/home-page.js
@@ -3,23 +3,39 @@
      const url = "https://threadgk.github.io/projects/part6/json/home-display.json"; 
      try {
         const response = await fetch(url); 
-        return await response.json(); 
+        const data = await response.json(); 
+        return data;
+
         } catch (error) {
             console.error("Error fetching gallery images!", error); 
+            return[];
         } 
 }; 
         const showSlideShow = async()=>{
                 const images = await getImages(); 
-                const slideShow = document.querySelector("#slideshow") ;  
 
-        images.forEach((image) => { 
-                const img = document.createElement("img"); 
-                 img.src = image.img ; 
-                 img.classList.add("slide"); 
-                if (index !== 0) img.classList.add("hidden");
-                 slideShow.appendChild(img) ; 
-}) ;    
-             setInterval=(()=> {
+                if (!Array.isArray(images) || images.length == 0) {
+                    console.error(" No images displaying!") ; 
+                    return ;
+                }
+
+        const slideShow = document.querySelector("#slideshow") ;   
+
+        images.forEach((item,index) => {
+
+            const imgUrl = Object.values(item)[0]; 
+
+            const img = document.createElement("img") ; 
+            img.src = imgUrl;
+            img.classList.add("slide"); 
+
+            if(index !== 0 ) img.classList.add("hidden"); 
+            
+            
+            slideShow.appendChild(img) ; 
+        }) ; 
+        
+            setInterval=(()=> {
 
                     const currentSlide = document.querySelector("#slideshow :not(.hidden)"); 
                     let nextSlide = currentSlide.nextElementSibling; 
@@ -31,4 +47,8 @@
                      currentSlide.classList.add("hidden"); 
                      nextSlide.classList.remove("hidden");
                   }, 10000) ; 
-                };showSlideShow();
+                 
+                    };    
+             
+                
+                showSlideShow();
